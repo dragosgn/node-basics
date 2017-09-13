3e8ab89854fb719caab92b200c38f00548ac5e98
Dragos Nedelcu <dragos.nedelcu@lokalleads.de>
2017-09-13 11:06:05 +0200

    basic express

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 var http = require('http')
-
+var express = require('express')
 
 http.createServer(function(request, response) {
   response.writeHead(200, {'Content-Type': 'text/plain'});
 
-  response.end('Hello World!')
+  response.end('Hello World')
 }).listen(3081);
 
 console.log('Server is running on localhost:3081')
