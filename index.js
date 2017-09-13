be86e7aade63e268a2d8af9d72a0363a06fb69a2
Dragos Nedelcu <dragos.nedelcu@lokalleads.de>
2017-09-13 11:30:30 +0200

    vent emittr

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 var http = require('http')
 var express = require('express')
 var fs = require('fs')
+var events = require('events')
 
 fs.readFile('input.txt', function(err, data) {
   if(err) return console.error(err);
   console.log(data.toString())
 })
+
+var eventEmitter = new events.EventEmitter()
+
+eventEmitter.on('eventName', eventHandler)
+
+eventEmitter.emit('eventName')
 
 http.createServer(function(request, response) {
   response.writeHead(200, {'Content-Type': 'text/plain'});
